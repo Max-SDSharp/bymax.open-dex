fca3ddc1ad362904fbfeddf11625225685d6110b
Maximiliano Salvatti <msalvatti@gmail.com>
2025-06-20 11:55:31 -0300

    refactor: adjust header layout for improved responsiveness and structure

--- src/app/terminal/header/index.tsx
+++ src/app/terminal/header/index.tsx
@@ -11,11 +11,13 @@ const SymbolSelection = dynamic(() => import('./symbolSelection'), {
 const HeaderInformation: React.FC = () => {
   return (
     <div className="w-full">
-      <div className="grid grid-cols-4 gap-4 mb-4">
-        <div className="col-span-1">
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+        <div className="col-span-1 md:mb-4">
           <SymbolSelection />
         </div>
-        <div className="col-span-3">{/* Other header info can go here */}</div>
+        <div className="col-span-1 md:col-span-3">
+          {/* Other header info can go here */}
+        </div>
       </div>
       <hr className="border-gray-600" />
     </div>
