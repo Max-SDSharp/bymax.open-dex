9bb52bdce5ec485af6cd1f5b6cf32a0720a5840f
Maximiliano Salvatti <msalvatti@gmail.com>
2025-06-20 09:11:25 -0300

    chore: update package dependencies and integrate Phantom and Solflare wallet adapters in SolanaProvider

--- src/providers/SolanaProvider.tsx
+++ src/providers/SolanaProvider.tsx
@@ -3,11 +3,13 @@
 import { ReactNode, useMemo } from 'react'
 
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom'
 import {
   ConnectionProvider,
   WalletProvider,
 } from '@solana/wallet-adapter-react'
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
+import { SolflareWalletAdapter } from '@solana/wallet-adapter-solflare'
 import { clusterApiUrl } from '@solana/web3.js'
 
 // Import the required CSS
@@ -26,9 +28,14 @@ export default function SolanaProvider({ children }: SolanaProviderProps) {
     return process.env.NEXT_PUBLIC_SOLANA_RPC_URL || clusterApiUrl(network)
   }, [network])
 
+  const wallets = useMemo(
+    () => [new PhantomWalletAdapter(), new SolflareWalletAdapter()],
+    [],
+  )
+
   return (
     <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={[]} autoConnect>
+      <WalletProvider wallets={wallets} autoConnect>
         <WalletModalProvider>{children}</WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
